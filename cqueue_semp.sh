cf2e385bd41176dc004acae64dec29e4b7611e2f
Vidyadhar Kothekar <vidyadhar@Vidyadhars-MacBook-Pro.local>
2015-10-22 02:16:04 +1100

    good one

--- cqueue_semp.sh
+++ cqueue_semp.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 IP=52.23.167.0:8080
 CRED=admin:admin
 VPN=testScript
 TOPIC=customer/create
 QUEUE=qCustAnalytics
+TOPIC1=a/b/c
+
 CREATE_QUEUE="\
 <rpc semp-version=\"soltr/6_1\">
     <message-spool>
@@ -56,6 +59,19 @@ SUB_TOPIC="\
     </message-spool>
 </rpc>\
 "
+SUB_TOPIC1="\
+<rpc semp-version=\"soltr/6_1\">
+    <message-spool>
+        <vpn-name>${VPN}</vpn-name>
+        <queue>
+            <name>${QUEUE}</name>
+            <subscription>
+                <topic>${TOPIC1}</topic>
+            </subscription>
+        </queue>
+    </message-spool>
+</rpc>\
+"
 QUEUE_START="\
 <rpc semp-version=\"soltr/6_1\">
     <message-spool>
@@ -73,4 +89,5 @@ echo "${CREATE_QUEUE}" | curl -d @- -u "${CRED}" http://${IP}/SEMP
 echo "${QUEUE_ACC}" | curl -d @- -u "${CRED}" http://${IP}/SEMP
 echo "${QUEUE_PERM}" | curl -d @- -u "${CRED}" http://${IP}/SEMP
 echo "${SUB_TOPIC}" | curl -d @- -u "${CRED}" http://${IP}/SEMP
+echo "${SUB_TOPIC1}" | curl -d @- -u "${CRED}" http://${IP}/SEMP
 echo "${QUEUE_START}" | curl -d @- -u "${CRED}" http://${IP}/SEMP
