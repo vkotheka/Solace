785b45885211802f0bd6ff6a682f9ac68c990ac4
Vidyadhar Kothekar <vidyadhar@Vidyadhars-MacBook-Pro.local>
2015-10-27 01:10:01 +1100

    good one

--- provision.sh
+++ provision.sh
@@ -1,1 +1,79 @@
-./NormalCustomerQueue.sh
+#!/bin/bash
+
+IP=52.91.89.215:8080
+CRED=admin:admin
+VPN=DataPowerDemo
+TOPIC=create/customer/vip
+QUEUE=VIPCustomer
+
+CREATE_QUEUE="\
+<rpc semp-version=\"soltr/6_1\">
+    <message-spool>
+        <vpn-name>${VPN}</vpn-name>
+        <create>
+            <queue>
+                <name>${QUEUE}</name>
+            </queue>
+        </create>
+    </message-spool>
+</rpc>\
+"
+QUEUE_ACC="\
+<rpc semp-version=\"soltr/6_1\">
+    <message-spool>
+        <vpn-name>${VPN}</vpn-name>
+        <queue>
+            <name>${QUEUE}</name>
+            <access-type>
+                <non-exclusive></non-exclusive>
+            </access-type>
+        </queue>
+    </message-spool>
+</rpc>\
+"
+QUEUE_PERM="\
+<rpc semp-version=\"soltr/6_1\">
+    <message-spool>
+        <vpn-name>${VPN}</vpn-name>
+        <queue>
+            <name>${QUEUE}</name>
+            <permission>
+                <all></all>
+                <delete></delete>
+            </permission>
+        </queue>
+    </message-spool>
+</rpc>\
+"
+SUB_TOPIC="\
+<rpc semp-version=\"soltr/6_1\">
+    <message-spool>
+        <vpn-name>${VPN}</vpn-name>
+        <queue>
+            <name>${QUEUE}</name>
+            <subscription>
+                <topic>${TOPIC}</topic>
+            </subscription>
+        </queue>
+    </message-spool>
+</rpc>\
+"
+
+QUEUE_START="\
+<rpc semp-version=\"soltr/6_1\">
+    <message-spool>
+        <vpn-name>${VPN}</vpn-name>
+        <queue>
+            <name>${QUEUE}</name>
+            <no>
+                <shutdown></shutdown>
+            </no>
+        </queue>
+    </message-spool>
+</rpc>\
+"
+echo "${CREATE_QUEUE}" | curl -d @- -u "${CRED}" http://${IP}/SEMP
+echo "${QUEUE_ACC}" | curl -d @- -u "${CRED}" http://${IP}/SEMP
+echo "${QUEUE_PERM}" | curl -d @- -u "${CRED}" http://${IP}/SEMP
+echo "${SUB_TOPIC}" | curl -d @- -u "${CRED}" http://${IP}/SEMP
+echo "${QUEUE_START}" | curl -d @- -u "${CRED}" http://${IP}/SEMP
